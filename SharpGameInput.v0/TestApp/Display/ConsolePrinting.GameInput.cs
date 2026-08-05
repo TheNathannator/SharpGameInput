a1286ff166dabb8a9205f7baa93b40aadc0f458b
Nathan <nathanhurst03@gmail.com>
2026-08-05 02:44:23 -0600

    Tweak device info printing in test app

--- SharpGameInput.v0/TestApp/Display/ConsolePrinting.GameInput.cs
+++ SharpGameInput.v0/TestApp/Display/ConsolePrinting.GameInput.cs
@@ -7,19 +7,22 @@ namespace SharpGameInput.TestApp.Display
         public static unsafe void Print(in GameInputDeviceInfo info)
         {
             Console.WriteLine($"- Name: {GameInputString.ToString(info.displayName)}");
+            Console.WriteLine($"- Interface number: {info.interfaceNumber}");
+            Console.WriteLine($"- Collection number: {info.collectionNumber}");
+            Console.WriteLine($"-----");
             Console.WriteLine($"- Hardware IDs: VID_{info.vendorId:X4}&PID_{info.productId:X4}&REV_{info.revisionNumber:X4}");
+            Console.WriteLine($"- Usage: {info.usage.page:X4}:{info.usage.id:X4}");
+            Console.WriteLine($"- Hardware version: {info.hardwareVersion}");
+            Console.WriteLine($"- Firmware version: {info.firmwareVersion}");
+            Console.WriteLine($"-----");
             Console.WriteLine($"- Device ID:      {info.deviceId}");
             Console.WriteLine($"- Device root ID: {info.deviceRootId}");
-
+            Console.WriteLine($"-----");
             Console.WriteLine($"- Family: {info.deviceFamily}");
             Console.WriteLine($"- Capabilities: {info.capabilities}");
             Console.WriteLine($"- Supported inputs: {info.supportedInput}");
-            Console.WriteLine($"- Hardware version: {info.hardwareVersion}");
-            Console.WriteLine($"- Firmware version: {info.firmwareVersion}");
-            Console.WriteLine($"- Interface number: {info.interfaceNumber}");
-            Console.WriteLine($"- Collection number: {info.collectionNumber}");
-            Console.WriteLine($"- Usage: {info.usage.page:X4}:{info.usage.id:X4}");
-
+            Console.WriteLine($"- Supported rumble: {info.supportedRumbleMotors}");
+            Console.WriteLine($"-----");
             Console.WriteLine($"- {info.deviceStringCount} strings");
             if (info.deviceStrings != null)
             {
@@ -28,15 +31,13 @@ namespace SharpGameInput.TestApp.Display
                     Console.WriteLine($"  - {info.deviceStrings[i]}");
                 }
             }
-
+            Console.WriteLine($"-----");
             Console.WriteLine($"- Descriptor data: {info.deviceDescriptorSize} bytes");
             if (info.deviceDescriptorData != null && info.deviceDescriptorSize > 0)
             {
                 var descriptor = new ReadOnlySpan<byte>(info.deviceDescriptorData, (int)info.deviceDescriptorSize);
                 PrintBufferWrapped(descriptor, indentAmount: 2, wrapCount: 32);
             }
-
-            // TODO: Other data
         }
     }
 }
